9661026e57af15a2af19e56e4d8fb39a0115a9f9
Chris Rathermel <gargy66@gmail.com>
2015-07-03 19:18:42 -0500

    added file watcher sample starting point

---
+++ FileWatcher/app.js
@@ -0,0 +1,12 @@
+﻿console.log('Node File Watcher!');
+
+var __dirname = 'C:\\Temp\\';
+
+var chokidar = require('chokidar');
+
+// One-liner for current directory, ignores .dotfiles
+chokidar.watch(__dirname, { ignored: /[\/\\]\./ }).on('all', function (event, path) {
+    console.log(event, path);
+});
+
+console.log('Watching Directory: ' + __dirname);
